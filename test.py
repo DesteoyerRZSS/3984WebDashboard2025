ce1851f6d127c2dab82281eb775f2a2171946943
Ryan Zheng <rzheng1232@gmail.com>
2025-03-17 15:48:59 -0400

    d

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import QUrl
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile
+from PyQt6.QtWebEngineWidgets import QWebEngineView
 import sys
 
 class WebApp(QMainWindow):
@@ -10,24 +10,8 @@ class WebApp(QMainWindow):
         # Set up the QWebEngineView to display HTML content
         self.browser = QWebEngineView()
 
-        # Simple HTML content
-        html_content = """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Simple HTML in PyQt</title>
-        </head>
-        <body>
-            <h1>Hello, PyQt6 WebEngine!</h1>
-            <p>This is a simple HTML page rendered inside the PyQt window.</p>
-        </body>
-        </html>
-        """
-
-        # Set the HTML content in the browser
-        self.browser.setHtml(html_content)
+        # Load a simple webpage
+        self.browser.setUrl(QUrl("https://www.google.com"))
 
         # Set the browser as the central widget of the window
         self.setCentralWidget(self.browser)
